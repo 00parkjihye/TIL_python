fbb251cb1045ded94b0ef9eed5f07ac1be7c9adf
ParkJihye <00park.jihye@gmail.com>
2021-01-18 11:23:55 +0900

    Add Set part5
    Add Set part5(Set functions)

--- General.py
+++ General.py
@@ -988,6 +988,24 @@ print(s1.difference(s2))  # {1, 2, 3}
 print(s2.difference(s1))  # {8, 9, 7}
 
 
+# 3) 집합 자료형 관련 함수들
+# 값 1개 추가(add): '.add()'  이미 만들어진 set 자료형에 값 추가.
+# 1개의 값만 추가 할 경우
+s1 = set([1, 2, 3])
+s1.add(4)
+print(s1)  # {1, 2, 3, 4}
+
+# 값 여러 개 추가(update): '.update()'
+s1 = set([1, 2, 3])
+s1.update([4, 5, 6])
+print(s1)  # {1, 2, 3, 4, 5, 6}
+
+# 특정 값 제거(remove): '.remove()'
+s1 = set([1, 2, 3])
+s1.remove(2)
+print(s1)  # {1, 3}
+
+
 # [추상화(Abstraction)]
 # 1> 변수(Variable): '값'을 저장.
 # 예시1)
